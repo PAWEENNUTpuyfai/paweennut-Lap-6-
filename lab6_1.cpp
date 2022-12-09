780c67aaf0cc73084a3b1f5ecea60af3e5f85ae4
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2022-12-09 16:10:31 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
+	int N=20;
+	int I[N];
+    int i=0;
+    do { 
+		cout << "Enter an integer: ";
+		cin >> I[i];
+	    i++;
+	}
+	while (I[i-1]!=0);
+	int n= 0;
+	int a=0;
+	int b=0;
+	while (n<i){
+		if (I[n]%2==0 ){
+			a=a+1;
+		}else{
+			b=b+1;
+		}
+		n++;
+	}
     cout << "#Even numbers = ";
+    cout << a-1<<"\n";
     cout << "#Odd numbers = ";
+    cout << b;
     return 0;
+    
 }
